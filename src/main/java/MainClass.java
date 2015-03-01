8dc1c2a303364db681c8ff5f151d276dda246548
bahuma20 <bahuma20@bahuma.me>
2015-03-01 18:53:06 +0100

    Initial Commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,20 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Setup Twitter Client
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setOAuthConsumerKey("5OxuX4YwJdxD433ijgo6DTtV9");
+        cb.setOAuthConsumerSecret("l8UOZGaBdndeS2eVhB30FvG3vry48tNVV6hJEK3fXqj1BQ1xf4");
+        cb.setOAuthAccessToken("2397473258-CdEacWH3PWF2GW5KWOUE02Jh6Tdvj8kAOWR2bdk");
+        cb.setOAuthAccessTokenSecret("AZRmze08tJWlSHGBzUpdxOYZVkobYw4JyZu4DxXNgvRxN");
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+
+        Twitter twitter = tf.getInstance();
+
+        new SendMessageFrame(twitter);
+    }
+}
---
+++ src/main/java/SendMessageFrame.java
@@ -0,0 +1,54 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SendMessageFrame extends JFrame {
+    private Twitter twitter;
+
+    private JLabel lblMessage;
+    private JTextArea txtaText;
+    private JButton btnSend;
+
+    public SendMessageFrame(Twitter twitter) {
+        this.twitter = twitter;
+
+        // Prepare Window
+        this.setTitle("Twitter Client");
+        this.setLocationRelativeTo(null);
+        this.setSize(500, 400);
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Setup Components
+        this.lblMessage = new JLabel("Nachricht:");
+        this.txtaText = new JTextArea();
+        this.btnSend = new JButton("Senden");
+
+        // Add components
+        this.add(lblMessage, BorderLayout.PAGE_START);
+        this.add(txtaText, BorderLayout.CENTER);
+        this.add(btnSend, BorderLayout.PAGE_END);
+
+        // Send Message
+        this.btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+
+        this.setVisible(true);
+    }
+
+    private void sendMessage() {
+        try {
+            twitter.sendDirectMessage("bahuma20", this.txtaText.getText());
+        } catch (TwitterException e1) {
+            e1.getMessage();
+        }
+    }
+}
