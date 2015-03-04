77fe829e1093804bc968c14199ef64d096bd62bb
bahuma20 <bahuma20@bahuma.me>
2015-03-04 21:38:07 +0100

    Open Browser with URL to Authorize

--- src/main/java/AuthorizationFrame.java
+++ src/main/java/AuthorizationFrame.java
@@ -1,7 +1,14 @@
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.auth.RequestToken;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class AuthorizationFrame extends JFrame {
     private Twitter twitter;
@@ -12,7 +19,7 @@ public class AuthorizationFrame extends JFrame {
     private JTextField txtPin;
     private JButton btnLogin;
 
-    public AuthorizationFrame(Twitter twitter, AuthorizationCallback callback) throws HeadlessException {
+    public AuthorizationFrame(Twitter twitter, final AuthorizationCallback callback) throws HeadlessException {
         this.twitter = twitter;
         this.callback = callback;
 
@@ -25,9 +32,24 @@ public class AuthorizationFrame extends JFrame {
         // Setup Compontents
         this.lblInfo = new JLabel("<html>Du musst dich einloggen, um diese Anwendung nutzen zu können. Fordern Sie eine PIN an, und geben Sie diese im Feld unten ein.</html>");
         this.lblInfo.setMaximumSize(new Dimension(500, 50));
+
         this.btnGetPin = new JButton("Get PIN");
+        this.btnGetPin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                getPin();
+            }
+        });
+
         this.txtPin = new JTextField();
+
         this.btnLogin = new JButton("Login");
+        this.btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
 
         // Add Components
         this.add(lblInfo);
@@ -42,6 +64,22 @@ public class AuthorizationFrame extends JFrame {
     public interface AuthorizationCallback {
         public void success(String AccessToken, String AccessSecret);
         public void error(Exception e);
+    }
+
+    private void getPin() {
+        try {
+            RequestToken requestToken = this.twitter.getOAuthRequestToken();
+            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                desktop.browse(new URI(requestToken.getAuthorizationURL()));
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
