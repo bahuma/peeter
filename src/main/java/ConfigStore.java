e2e11ca167ed044d3276f82887e15d5df9b6a585
bahuma20 <bahuma20@bahuma-online.de>
2015-03-04 15:29:18 +0100

    Added ConfigStore

---
+++ src/main/java/ConfigStore.java
@@ -0,0 +1,15 @@
+public class ConfigStore {
+    private String configPath;
+
+    public ConfigStore() {
+
+    }
+
+    public String get(String name) {
+        return "";
+    }
+
+    public String set(String name, String value) {
+        return "";
+    }
+}
