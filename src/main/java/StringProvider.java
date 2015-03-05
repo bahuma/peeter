c9b3601451698e8bf0029127c3ef4192afcf8604
bahuma20 <bahuma20@bahuma-online.de>
2015-03-05 09:58:25 +0100

    Provide some vars

--- src/main/java/StringProvider.java
+++ src/main/java/StringProvider.java
@@ -1,4 +1,17 @@
+import sun.reflect.Reflection;
+
+import java.io.File;
+import java.util.Properties;
+
 public class StringProvider {
+    File stringFile;
+    String language;
+    Properties strings;
+
+    public StringProvider(String language) {
+        this.language = language;
+    }
+
     public String get(String id) {
         return "";
     }
