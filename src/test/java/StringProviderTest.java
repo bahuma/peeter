c514541869ff93d4fbfa8ca2845ee4627395892d
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2015-03-09 15:39:24 +0100

    Coverted to JUnit 4 Test

--- src/test/java/StringProviderTest.java
+++ src/test/java/StringProviderTest.java
@@ -1,29 +1,30 @@
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.Locale;
 
-public class StringProviderTest extends TestCase {
+public class StringProviderTest {
 
+    @Test
     public void testGet() throws Exception {
-        StringProvider prov = new StringProvider();
-
-        assertEquals("String \"test.first\" should be \"Erster Test\"", "Erster Test", prov.get("test.first"));
-        assertEquals("String \"test.second\" should be \"Zweiter Test\"", "Zweiter Test", prov.get("test.second"));
-    }
-
-    public void testUpdateLocale() throws Exception {
         StringProvider prov = new StringProvider();
         ConfigStore cs = new ConfigStore();
 
-        assertEquals("String \"test.first\" should be \"Erster Test\"", "Erster Test", prov.get("test.first"));
-        assertEquals("String \"test.second\" should be \"Zweiter Test\"", "Zweiter Test", prov.get("test.second"));
+        cs.set("locale.language", "de");
+        cs.set("locale.country", "US");
+
+        prov.updateLocale();
+
+        Assert.assertTrue("String \"test.first\" should be \"Erster Test\"", prov.get("test.first").equals("Erster Test"));
+        Assert.assertTrue("String \"test.second\" should be \"Zweiter Test\"", prov.get("test.second").equals("Zweiter Test"));
 
         cs.set("locale.language", "en");
         cs.set("locale.country", "US");
 
         prov.updateLocale();
 
-        assertEquals("String \"test.first\" should now be \"First Test\"", "First Test", prov.get("test.first"));
-        assertEquals("String \"test.second\" should now be \"Second Test\"", "Second Test", prov.get("test.second"));
+        Assert.assertTrue("String \"test.first\" should now be \"First Test\"", prov.get("test.first").equals("First Test"));
+        Assert.assertTrue("String \"test.second\" should now be \"Second Test\"", prov.get("test.second").equals("Second Test"));
     }
 }
